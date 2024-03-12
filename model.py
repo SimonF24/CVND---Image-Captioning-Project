4b83133f3d91fc8aade4f054bde5365bf5649ff2
Simon Fraser <36797898+SimonF24@users.noreply.github.com>
2024-03-11 21:57:31 -0700

    Adding my work

--- model.py
+++ model.py
@@ -2,11 +2,11 @@ import torch
 import torch.nn as nn
 import torchvision.models as models
 
-
 class EncoderCNN(nn.Module):
     def __init__(self, embed_size):
         super(EncoderCNN, self).__init__()
-        resnet = models.resnet50(pretrained=True)
+        # Using ResNet-34 for a lighter model
+        resnet = models.resnet18(pretrained=True)
         for param in resnet.parameters():
             param.requires_grad_(False)
         
@@ -19,15 +19,34 @@ class EncoderCNN(nn.Module):
         features = features.view(features.size(0), -1)
         features = self.embed(features)
         return features
-    
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
-    
+        super(DecoderRNN, self).__init__()
+        # TODO: Complete this function
+        self.hidden_size = hidden_size
+        self.dropout = nn.Dropout(0.2)
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers=num_layers, batch_first=True)
+        self.linear = nn.Linear(hidden_size, vocab_size)
+
     def forward(self, features, captions):
-        pass
+        embeddings = self.embed(captions[:, :-1])  # Exclude the <end> token
+        # TODO: Complete this function
+        features_and_embeddings = torch.cat((features.unsqueeze(1), embeddings), dim=1)
+        hiddens, states = self.lstm(features_and_embeddings)
+        hiddens = self.dropout(hiddens)
+        outputs = self.linear(hiddens)
+        return outputs
 
     def sample(self, inputs, states=None, max_len=20):
-        " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        pass
+        "accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len)"
+        states = (torch.randn(1, 1, 512).to(inputs.device), torch.randn(1, 1, 512).to(inputs.device))
+        predicted_sentence = []
+        for i in range(max_len):
+            hiddens, states = self.lstm(inputs, states)
+            outputs = self.linear(hiddens.squeeze(1))
+            _, predicted = outputs.max(1)
+            predicted_sentence.append(predicted.item())
+            inputs = self.embed(predicted).unsqueeze(1)
+        return predicted_sentence
